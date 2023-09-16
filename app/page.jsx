0f1f43d12f274684c3db692015ce0c350931152c
Oliver <opaucarq@uni.pe>
2023-09-15 22:37:35 -0500

    add: home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1>Home Page</h1>;
+import Users from "@/components/Users";
+import fetchUsers from "@/api/users";
+
+export default async function Home() {
+  const users = await fetchUsers();
+
+  return <Users users={users} />;
 }
